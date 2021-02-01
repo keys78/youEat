dd7bbd3a64d2419914a3c7e532240f3b64dfa1fc
unknown <rahzy24@gmail.com>
2021-02-01 21:55:59 +0100

    youEat. An e-commerce sharwama procurement website

---
+++ login.js
@@ -0,0 +1,71 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+
+
+
+  //*************************ABUJA LOCATIONS ******************** */
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+ //*************************LAGOS LOCATIONS ******************** */
+  function myiFunction() {
+    var x = document.getElementById("myLnks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  
+ //*************************Port LOCATIONS ******************** */
+  function myphiFunction() {
+    var x = document.getElementById("phmyLnks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+
+
+ //*************************Owerri LOCATIONS ******************** */
+  function myotiFunction() {
+    var x = document.getElementById("otmyLnks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+ //*************************Abia LOCATIONS ******************** */
+  function myabiFunction() {
+    var x = document.getElementById("abmyLnks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
